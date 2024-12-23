aff1a9de4290ddcc4c8cc77837e8058bcdf46adf
Mina Nazari <mina.nazary66@gmail.com>
2024-12-23 22:43:21 +0330

    file's address directories edited for css ind spinner

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import spinnerImage from "../assets/images/spinner.gif"
 
 const Spinner = () => {
   return (
     <div className="container">
       <div className="row">
         <div className='d-flex justify-content-between align-items-center'>
-          <img src={spinnerImage} style={{ width: "300px", margin: "0 auto"}} />
+          <img src="/images/spinner.gif" style={{ width: "300px", margin: "0 auto"}} />
         </div>
       </div>
     </div>
