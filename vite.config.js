eaa09daf5d6d9a36844e00f7e87f401e2b7646e7
Mina Nazari <mina.nazary66@gmail.com>
2024-12-24 10:50:42 +0330

    remove base for testing url working

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './',
   build: { outDir: 'dist' },
   plugins: [react()],
   assetsInclude: ['**/*.JPG', '**/*.jpg'],
   server: {
-    proxy: { '/api': 'http://localhost:3001' }
+    proxy: { '/api': 'http://localhost:3001' },
+    mimeTypes: {
+      '.js': 'application/javascript'
+    }
   }
 })
 
